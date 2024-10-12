95009826ee8792b016ded179722bfaebefd5303e
Mikael Sundell <mikael.sundell@gmail.com>
2024-10-12 20:36:45 +0200

    Improved build_app.sh

--- build_app.sh
+++ build_app.sh
@@ -10,66 +10,40 @@ major_version=$(echo "$macos_version" | cut -d '.' -f 1)
 
 # signing
 sign_code=OFF
+developerid_identity=""
 mac_developer_identity=""
 mac_installer_identity=""
+provisioning_profile=""
+provisioning_profile_path=""
 
 sign_app() {
     local bundle_path="$1"
-    local merge_path="$2"
-    local sign_type="$3"
-    local sign_identity="$4"
+    local sign_type="$2"
+    local sign_identity="$3"
 
     case "$sign_type" in
         dylibs)
             find "$bundle_path" -type f ! -path "*.framework/*" | while read -r file; do
                 file_type=$(file "$file")
                 if [[ "$file_type" == *"Mach-O 64-bit dynamically linked shared library"* ]] || [[ "$file_type" == *"Mach-O 64-bit bundle"* ]]; then
-                    merge_file="${file/$bundle_path/$merge_path}"
-                    if [ -f "$merge_file" ]; then
-                        echo "merging dylib $file with $merge_file version ..."
-                        lipo -create "$file" "$merge_file" -output "$file.tmp"
-                        mv "$file.tmp" "$file"
-                    fi
                     echo "signing dylib $file ..."
                     codesign --force --sign "$sign_identity" --timestamp "$file"
-                    echo ""
                 fi
             done
             ;;
         frameworks)
             find "$bundle_path" -type d -name "*.framework" | while read -r framework; do
-                merge_framework="${framework/$bundle_path/$merge_path}"
-                if [ -d "$merge_framework" ]; then
-                    echo "merging framework $framework with $merge_framework version ..."
-                    for binary in "$framework"/Versions/A/*; do
-                        merge_binary="${binary/$framework/$merge_framework}"
-                        if [ -f "$merge_binary" ]; then
-                            echo "merging $binary with $merge_binary version ..."
-                            lipo -create "$binary" "$merge_binary" -output "$binary.tmp"
-                            mv "$binary.tmp" "$binary"
-                        fi
-                    done
-                else
-                    echo -e "framework $merge_framework does not exist, will not be merged"
-                fi
                 echo "signing framework $framework ..."
                 codesign --force --sign "$sign_identity" --timestamp "$framework"
-                echo ""
             done
             ;;
         executables)
             find "$bundle_path" -type f | while read -r file; do
                 file_type=$(file "$file")
                 if [[ "$file_type" == *"Mach-O 64-bit executable"* ]]; then
-                    merge_file="${file/$bundle_path/$merge_path}"
-                    if [ -f "$merge_file" ]; then
-                        echo "merging $file with $merge_file version ..."
-                        lipo -create "$file" "$merge_file" -output "$file.tmp"
-                        mv "$file.tmp" "$file"
-                    fi
                     echo "signing executable $file with entitlements ..."
+                    echo codesign --force --sign "$sign_identity" --timestamp --options runtime --entitlements "$script_dir/resources/App.entitlements" "$file"
                     codesign --force --sign "$sign_identity" --timestamp --options runtime --entitlements "$script_dir/resources/App.entitlements" "$file"
-                    echo ""
                 fi
             done
             ;;
@@ -86,12 +60,9 @@ verify_app() {
         echo "verifying $file..."
         if codesign --verify --verbose "$file"; then
             echo "signature verification passed for $file"
-            echo "permissions updated for $file"
-            chmod o+r "$file"
         else
             echo "signature verification failed for $file"
         fi
-                echo ""
     done
 }
 
@@ -100,28 +71,33 @@ parse_args() {
     while [[ "$#" -gt 0 ]]; do
         case $1 in
             --target=*) 
-                major_version="${1#*=}" ;; 
+                major_version="${1#*=}" ;;
             --sign)
                 sign_code=ON ;;
+            --provisioning=*)
+                provisioning_profile="${1#*=}" ;;
+            --provisioningpath=*)
+                provisioning_profile_path="${1#*=}" ;;
+            --github)
+                github=ON ;;
+            --appstore)
+                appstore=ON ;;
             *)
-                if [[ "$1" == *".app" ]]; then
-                    build_apps+=("$1")
-                else
-                    echo "Unknown option or invalid argument: $1"
-                    exit 1
-                fi
+                build_type="$1" # save it in build_type if it's not a recognized flag
                 ;;
         esac
         shift
     done
-
-    # Check if three .app directories are provided
-    if [ "${#build_apps[@]}" -ne 3 ]; then
-        echo "Please provide three .app directories."
-        exit 1
-    fi
 }
 parse_args "$@"
+
+# target
+if [ -z "$major_version" ]; then
+    macos_version=$(sw_vers -productVersion)
+    major_version=$(echo "$macos_version" | cut -d '.' -f 1)
+fi
+export MACOSX_DEPLOYMENT_TARGET=$major_version
+export CMAKE_OSX_DEPLOYMENT_TARGET=$major_version
 
 # exit on error
 set -e 
@@ -129,7 +105,13 @@ set -e
 # clear
 clear
 
-echo "Building Automator for universal"
+# build type
+if [ "$build_type" != "debug" ] && [ "$build_type" != "release" ] && [ "$build_type" != "all" ]; then
+    echo "invalid build type: $build_type (use 'debug', 'release', or 'all')"
+    exit 1
+fi
+
+echo "Building Automator for $build_type"
 echo "---------------------------------"
 
 # signing
@@ -161,69 +143,141 @@ if [ "$sign_code" == "ON" ]; then
     echo ""
 fi
 
-# build automator
-build_automator() {
-    local build_app="$1"
-    local arm64_app="$2"
-    local x86_64_app="$3"
-
-    if [ ! -d "$arm64_app" ] || [ ! -d "$x86_64_app" ]; then
-        echo "arm64.app and x86_64.app required to build universal app."
+# check if cmake is in the path
+if ! command -v cmake &> /dev/null; then
+    echo "cmake not found in the PATH, will try to set to /Applications/CMake.app/Contents/bin"
+    export PATH=$PATH:/Applications/CMake.app/Contents/bin
+    if ! command -v cmake &> /dev/null; then
+        echo "cmake could not be found, please make sure it's installed"
         exit 1
     fi
+fi
 
-     pkg_file="$script_dir/Automator_macOS${major_version}_universal.pkg"
-    if [ -f "$pkg_file" ]; then
-        rm -f "$pkg_file"
+# check if cmake version is compatible
+if ! [[ $(cmake --version | grep -o '[0-9]\+\(\.[0-9]\+\)*' | head -n1) < "3.28.0" ]]; then
+    echo "cmake version is not compatible with Qt, must be before 3.28.0 for multi configuration"
+    exit 1;
+fi
+
+# build automator
+build_automator() {
+    local build_type="$1"
+
+    # cmake
+    export PATH=$PATH:/Applications/CMake.app/Contents/bin &&
+
+    # script dir
+    cd "$script_dir"
+
+    # clean dir
+    build_dir="$script_dir/build.$build_type"
+    if [ -d "$build_dir" ]; then
+        rm -rf "$build_dir"
     fi
 
-    if [ -d "$build_app" ]; then
-        read -p "build_app directory already exists, do you want to remove it before copying? (y/n): " remove_build_app
+    # build dir
+    mkdir -p "build.$build_type"
+    cd "build.$build_type"
 
-        if [[ "$remove_build_app" =~ ^[Yy]$ ]]; then
-            echo "removing the existing build_app directory ..."
-            rm -rf "$build_app"
-        else
-            echo "please remove or rename the existing build_app directory and try again."
-            exit 1
-        fi
+    # prefix dir
+    if ! [ -d "$THIRDPARTY_DIR" ]; then
+        echo "could not find 3rdparty project in: $THIRDPARTY_DIR"
+        exit 1
     fi
+    prefix="$THIRDPARTY_DIR"
 
-    cp -RP "$arm64_app" "$build_app"
+    # xcode build
+    xcode_type=$(echo "$build_type" | awk '{ print toupper(substr($0, 1, 1)) tolower(substr($0, 2)) }')
 
-    if [ -n "$mac_developer_identity" ]; then
-        if [ "$sign_code" == "ON" ]; then
-            # entitlements
-            teamid=$(echo "$mac_developer_identity" | awk -F '[()]' '{print $2}')
-            echo 
-            applicationid=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$build_app/Contents/Info.plist")
-            entitlements="resources/App.entitlements"
-            echo sed -e "s/\${TEAMID}/$teamid/g" -e "s/\${APPLICATIONIDENTIFIER}/$applicationid/g" "$script_dir/resources/App.entitlements.in" > "$entitlements"
-            sed -e "s/\${TEAMID}/$teamid/g" -e "s/\${APPLICATIONIDENTIFIER}/$applicationid/g" "$script_dir/resources/App.entitlements.in" > "$entitlements"
-            # sign
-            sign_app "$build_app" "$x86_64_app" "dylibs" "$mac_developer_identity"
-            sign_app "$build_app" "$x86_64_app" "frameworks" "$mac_developer_identity"
-            sign_app "$build_app" "$x86_64_app" "executables" "$mac_developer_identity"
-            verify_app "$build_app"
-            echo codesign --force --deep --sign "$mac_developer_identity" --entitlements $entitlements "$build_app"
-            codesign --force --deep --sign "$mac_developer_identity" --entitlements $entitlements "$build_app"
-            codesign --verify "$build_app"
-        fi
-    else 
-        echo "Mac Developer identity must be set for appstore distribution, sign will be skipped."
-    fi
-
-    # productbuild
-    if [ "$sign_code" == "ON" ]; then
-        if [ -n "$mac_installer_identity" ]; then
-            productbuild --component "$build_app" "/Applications" --sign "${mac_installer_identity}" --product "$build_app/Contents/Info.plist" "$pkg_file" 
-        else 
-            echo "Mac Installer identity must be set for appstore distribution, sign will be skipped."
-            productbuild --component "$build_app" "/Applications" --product "$build_app/Contents/Info.plist" "$pkg_file" 
-        fi  
+    # build
+    if [ -n "$provisioning_profile" ] && [ -n "$provisioning_profile_path" ]; then
+        cmake .. -DCMAKE_MODULE_PATH="$script_dir/modules" -DCMAKE_PREFIX_PATH="$prefix" -DPROVISIONING_PROFILE="$provisioning_profile" -G Xcode
     else
-        productbuild --component "$build_app" "/Applications" --product "$build_app/Contents/Info.plist" "$pkg_file" 
+        cmake .. -DCMAKE_MODULE_PATH="$script_dir/modules" -DCMAKE_PREFIX_PATH="$prefix" -G Xcode
+    fi
+    cmake --build . --config $xcode_type --parallel &&
+
+    if [ "$github" == "ON" ]; then
+        dmg_file="$script_dir/Automator_macOS${major_version}_${machine_arch}_${build_type}.dmg"
+        if [ -f "$dmg_file" ]; then
+            rm -f "$dmg_file"
+        fi
+
+        # deploy
+        $script_dir/scripts/macdeploy.sh -b "$xcode_type/Automator.app" -m "$prefix/bin/macdeployqt"
+
+        if [ -n "$developerid_identity" ]; then
+            if [ "$sign_code" == "ON" ]; then
+                codesign --force --deep --sign "$developerid_identity" --timestamp --options runtime "$xcode_type/Automator.app"
+            fi
+        else 
+            echo "Developer ID identity must be set for github distribution, sign will be skipped."
+        fi
+
+        # deploydmg
+        $script_dir/scripts/macdmg.sh -b "$xcode_type/Automator.app" -d "$dmg_file"
+        if [ -n "$developerid_identity" ]; then
+            if [ "$sign_code" == "ON" ]; then
+                codesign --force --deep --sign "$developerid_identity" --timestamp --options runtime --verbose "$dmg_file"
+            fi
+        else 
+            echo "Developer ID identity must be set for github distribution, sign will be skipped."
+        fi
+    fi
+
+    if [ "$appstore" == "ON" ]; then
+        pkg_file="$script_dir/Automator_macOS${major_version}_${machine_arch}_${build_type}.pkg"
+        if [ -f "$pkg_file" ]; then
+            rm -f "$pkg_file"
+        fi
+
+        # provisioning
+        if [ -n "$provisioning_profile" ] && [ -n "$provisioning_profile_path" ]; then
+            cp -f "$provisioning_profile_path" "$xcode_type/Automator.app/Contents/embedded.provisionprofile"
+        else
+            echo "Provisioning profile and path must be set for appstore distribution, will be skipped."
+        fi
+
+        # deploy
+        $script_dir/scripts/macdeploy.sh -b "$xcode_type/Automator.app" -m "$prefix/bin/macdeployqt"
+        if [ -n "$mac_developer_identity" ]; then
+            if [ "$sign_code" == "ON" ]; then
+                # entitlements
+                teamid=$(echo "$mac_developer_identity" | awk -F '[()]' '{print $2}')
+                applicationid=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$xcode_type/Automator.app/Contents/Info.plist")
+                entitlements="$script_dir/resources/App.entitlements"
+                echo sed -e "s/\${TEAMID}/$teamid/g" -e "s/\${APPLICATIONIDENTIFIER}/$applicationid/g" "$script_dir/resources/App.entitlements.in" > "$entitlements"
+                sed -e "s/\${TEAMID}/$teamid/g" -e "s/\${APPLICATIONIDENTIFIER}/$applicationid/g" "$script_dir/resources/App.entitlements.in" > "$entitlements"
+                # sign
+                sign_app "$xcode_type/Automator.app" "dylibs" "$mac_developer_identity"
+                sign_app "$xcode_type/Automator.app" "frameworks" "$mac_developer_identity"
+                sign_app "$xcode_type/Automator.app" "executables" "$mac_developer_identity"
+                verify_app "$xcode_type/Automator.app"
+                codesign --force --deep --sign "$mac_developer_identity" --entitlements $entitlements "$xcode_type/Automator.app"
+                codesign --verify "$xcode_type/Automator.app"
+            fi
+        else 
+            echo "Mac Developer identity must be set for appstore distribution, sign will be skipped."
+        fi
+
+        # productbuild
+        if [ "$sign_code" == "ON" ]; then
+            if [ -n "$mac_installer_identity" ]; then
+                productbuild --component "$xcode_type/Automator.app" "/Applications" --sign "${mac_installer_identity}" --product "$xcode_type/Automator.app/Contents/Info.plist" "$pkg_file" 
+            else 
+                echo "Mac Installer identity must be set for appstore distribution, sign will be skipped."
+                productbuild --component "$xcode_type/Automator.app" "/Applications" --product "$xcode_type/Automator.app/Contents/Info.plist" "$pkg_file" 
+            fi  
+        else
+            productbuild --component "$xcode_type/Automator.app" "/Applications" --product "$xcode_type/Automator.app/Contents/Info.plist" "$pkg_file" 
+        fi
     fi
 }
 
-build_automator s"${build_apps[@]}"
+# build types
+if [ "$build_type" == "all" ]; then
+    build_automator "debug"
+    build_automator "release"
+else
+    build_automator "$build_type"
+fi
